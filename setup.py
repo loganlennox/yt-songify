2fb0676bffb3611ca84cb40528777fce8a050daa
Logan Lennox <loganlennox@protonmail.com>
2018-06-24 20:36:09 -0400

    Add Project Structure

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name = "yt-songify",
+      version = "0.1",
+      description = "Command-line tool that downloads content from YouTube as mp3 files and adds proper metadata.",
+      url = "https://github.com/loganlennox/yt-songify",
+      author = "Logan Lennox",
+      author_email = "loganlennox@protonmail.com",
+      license = "MIT",
+      packages = ["yt-songify"],
+      include_package_data = True,
+      install_requires = [
+          "youtube-dl",
+          "mutagen",
+      ],
+      zip_safe = False)
